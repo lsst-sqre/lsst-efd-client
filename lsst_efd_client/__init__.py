09c98a893cd67431345ab1cb06c6c82cd875057f
Simon Krughoff <simon.krughoff@gmail.com>
2019-12-31 21:08:05 +0000

    Bump version: 0.1.6 → 0.1.7

--- lsst_efd_client/__init__.py
+++ lsst_efd_client/__init__.py
@@ -1,7 +1,7 @@
 """
 Collection of EFD utilities
 """
-__version__ = "__version__ = '0.1.6'"
+__version__ = "__version__ = '0.1.7'"
 from .auth_helper import NotebookAuth
 from .efd_helper import EfdClient, resample
 
