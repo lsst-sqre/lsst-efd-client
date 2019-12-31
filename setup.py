464ed9040e4b70de1952fa0d0b7ba4b0f79b645c
Simon Krughoff <simon.krughoff@gmail.com>
2019-12-31 17:52:37 +0000

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,22 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [
+                'numpy>=1.16.0',
+                'aioinflux',
+                'pandas',
+                'astropy', ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+                      'pip==19.2.3',
+                      'bump2version==0.5.11',
+                      'wheel==0.33.6',
+                      'watchdog==0.9.0',
+                      'flake8==3.7.8',
+                      'coverage==4.5.4',
+                      'Sphinx==1.8.5',
+                      'twine==1.14.0',
+                      'pytest-runner', ]
 
 test_requirements = ['pytest>=3', ]
 
