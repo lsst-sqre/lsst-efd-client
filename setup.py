af2e09473ab8136961da0d2e288cc61e6d60825f
Simon Krughoff <simon.krughoff@gmail.com>
2020-01-31 14:12:13 -0700

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -64,6 +64,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extra_requirements,
     url='https://github.com/lsst-sqre/lsst-efd-client',
-    version='0.2.3',
+    version='0.3.0',
     zip_safe=False,
 )
