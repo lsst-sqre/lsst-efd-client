defbf90274c36c700753689f59813d9c5030ee1a
Jonathan Sick <jsick@lsst.org>
2023-07-06 19:03:56 -0400

    Use importlib.metadata to get version

--- src/lsst_efd_client/__init__.py
+++ src/lsst_efd_client/__init__.py
@@ -1,7 +1,7 @@
-"""
-Collection of EFD utilities
-"""
-__version__ = "__version__ = '0.13.1'"
+"""Collection of EFD utilities."""
+
+import importlib.metadata
+
 from .auth_helper import NotebookAuth
 from .efd_helper import EfdClient
 from .efd_utils import (
@@ -17,3 +17,8 @@ __all__ = [
     "rendezvous_dataframes",
     "merge_packed_time_series",
 ]
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
